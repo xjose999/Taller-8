60c0912439cb9cb429f44a0940464a147e5b4522
xjose999 <josejoestar80@gmail.com>
2026-05-29 22:16:27 -0500

    Ejercicio 3

---
+++ Empleado.java
@@ -0,0 +1,9 @@
+class Empleado {
+    String nombre;
+    double salario;
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Salario: " + salario);
+    }
+}
