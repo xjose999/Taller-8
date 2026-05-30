3a83549d00e2ec52ce4e3cb79d08759fc57db2cd
xjose999 <josejoestar80@gmail.com>
2026-05-29 22:09:29 -0500

    Ejercicio 1

---
+++ Vehiculo.java
@@ -0,0 +1,9 @@
+class Vehiculo {
+    String marca;
+    int velocidadMaxima;
+
+    public void mostrarVehiculo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Velocidad: " + velocidadMaxima);
+    }
+}
